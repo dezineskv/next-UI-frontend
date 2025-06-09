4c369b3773c3ba44d4b13d70536c23b561842ab6
KimV <71965590+dezineskv@users.noreply.github.com>
2025-06-08 19:24:58 -0700

    initial nextJs tailwind daisyUI

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,12 @@
+import React from "react";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <>
-      <div className="justify-center mx-auto my-10 pb-10 text-center pt-5">
-        <figure className="max-w-[400px] justify-center mx-auto">
+      <div className="flex flex-col justify-center mx-auto my-10 pb-10 text-center pt-5">
+        <figure
+         className="max-w-[400px] justify-center mx-auto">
           <img
             src="https://img.freepik.com/vrije-vector/pagina-niet-gevonden-concept-illustratie_114360-1869.jpg"
             alt="not found image"
@@ -14,8 +16,10 @@ export default function NotFound() {
           Page Not Found
         </h3>
         <p className="text-sm">Could not find requested resource</p>
-        <br/>
-        <Link href="/" className="mt-10 text-sm underline">RETURN HOME</Link>
+        <br />
+        <Link href="/" className="mt-10 text-sm underline">
+          RETURN HOME
+        </Link>
       </div>
     </>
   );
