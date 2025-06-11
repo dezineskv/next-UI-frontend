026aa68ff6715b0299ce35ea985a02e1545e6dd8
KimV <71965590+dezineskv@users.noreply.github.com>
2025-06-11 16:09:13 -0700

    adjustment to image module-added mern img

--- src/app/(mainLayout)/about/page.tsx
+++ src/app/(mainLayout)/about/page.tsx
@@ -9,19 +9,21 @@ const AboutUs = () => {
       <div className="hero bg-base-200 my-20">
         <div className="hero-content flex-col lg:flex-row">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            src="https://th.bing.com/th/id/OIP.2FmoP_hZ7etc0kNf6U3epQHaEK?rs=1&pid=ImgDetMain"
             className="max-w-sm rounded-lg shadow-2xl"
           />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News</h1>
-            <p className="py-6">
+            <h2 className="text-5xl font-bold text-black md: pl-10">In the News</h2>
+            <h3 className="py-6 text-black md: pl-10">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
-            </p>
-            <a href="/about">
-              <button className="btn btn-primary">Get Started</button>
-            </a>{" "}
+            </h3>
+            <div className="md: pl-10">
+              <a href="/about">
+                <button className="btn btn-primary">Get Started</button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
